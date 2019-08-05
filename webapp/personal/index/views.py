8d7bce2f7297588d0776ac4a1619593dbebf5aa4
aditj <aditjain1980@gmail.com>
2019-08-05 15:59:47 +0530

    csrf for webhook exempted --correctio

--- webapp/personal/index/views.py
+++ webapp/personal/index/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from .models import Project,Article,Interest,Book,Contact
+from django.views.decorators.csrf import csrf_exempt
+
 import git
 # Create your views here.
 def index(request):
