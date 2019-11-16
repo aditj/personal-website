9d9e59895724fd1444b8b3d72539a6cc121bedeb
aditj <aditjain1980@gmail.com>
2019-11-16 23:12:35 +0530

    Added some projects

--- webapp/personal/index/models.py
+++ webapp/personal/index/models.py
@@ -5,13 +5,13 @@ from datetime import date
 class Book(models.Model):
     name=models.CharField(max_length=100)
     author=models.CharField(max_length=100)
-    read_on=models.DateField('Date Read On')
+    read_on=models.DateField('Date Read On',null=True)
     review=models.TextField(blank=True,null=True)
 class Article(models.Model):
     article_id=models.AutoField(primary_key=True)
     about=models.TextField(null=True)
     topic=models.CharField(max_length=200)
-    written_on=models.DateField('Written On',default=date.today )
+    written_on=models.DateField('Written On',default=date.today,null=True )
     content=models.FileField(upload_to='articles/')
 class Skill(models.Model):
     name =models.CharField(max_length=100)
@@ -21,8 +21,8 @@ class Interest(models.Model):
 class Project(models.Model):
     name=models.CharField(max_length=200)
     url=models.URLField(null=True,blank = True)
-    desc=models.TextField()
-    report=models.URLField(null=True)
+    desc=models.TextField(null=True)
+    report=models.URLField(null=True,blank=True)
     image=models.ImageField(upload_to="images/projects/",null=True, blank=True)
     type=models.CharField(max_length=100)
     done_on=models.DateField('Done On',default=date.today )
